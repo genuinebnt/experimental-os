b91c9629de9a73ca047966b7b646869b9874a2bd
genuine <genuinebnt@gmail.com>
2023-08-15 21:11:48 +0530

    Added Global descriptor table

--- kernel/arch/i386/boot.asm
+++ kernel/arch/i386/boot.asm
@@ -19,9 +19,28 @@ resb 16384
 stack_top:
 
 section .text
+global gdt_flush
+extern gp
+gdt_flush:
+  lgdt [gp]
+  mov ax, 0x10
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  jmp 0x08:flush2
+
+flush2:
+  ret
+
 global _start: function (_start.end - _start)
 _start:
   mov esp, stack_top
+  ; setup gdt
+  extern set_gdt
+  call set_gdt
+  ; call kernel_main
   extern kernel_main
   call kernel_main
 
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -12,13 +12,5 @@
 
 void kernel_main()
 {
-    terminal_init();
-    char *src = "hello world";
-    char dst[14];
-    int bytes = strncpy(dst, src, strlen(src));
-    printf("Destination buffer: %s\n Source buffer: %s\n Bytes copied: %d", dst, src, bytes);
-    for (size_t i = 0; i < 50; i++)
-    {
-        printf("Hello %d\n", i);
-    }
+    printf("Hello world!\n");
 }
